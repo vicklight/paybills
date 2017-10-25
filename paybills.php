a1f7de97b73fea9d2fd57160e3a4bd2a8da29766
Victor Nwauwa <victor.nwauwa93@gmail.com>
2017-10-25 15:02:41 -0700

    Add files via upload

--- paybills.php
+++ paybills.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
-    <title>Invite friends</title>
+    <title>Pay Bills</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/paybills.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -10,35 +10,33 @@
     <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-    <div class="container paybills-con">
-        <div class="col col-sm-12 col-xs-6 con">
-        <div class="row ">
+    <div class="container con">
+                <div class="row ">
             <div class="paybills-head col-sm-8 col-xs-8 ">
                 <h3 class="panel-heading">Pay Bills</h3>
             </div>
         </div>
 
-        <div class="row ">
-            <div class="  paybills-body col-sm-8 col-xs-8 ">
                 <div class="row ">
+            <div class="  paybills-body col-sm-8 col-xs-8 ">
+                <div class="row buttons1 ">
                     <div id="paybills-entery1">
-                        <h4 class="entery">CABLE TV Subscription</h4>
+                        <button class="btn entery">CABLE TV Subscription</button>
                     </div>
                     <div id="paybills-entery2">
-                        <h4 class="entery">Utility Bills</h4>
+                        <button class="btn entery">Data Bundle</button>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row buttons2">
                     <div id="paybills-entery3">
-                        <h4 class="entery">Data Bundle</h4>
+                        <button class="btn entery">Utility Bills</button>
                     </div>
                     <div id="paybills-entery4">
-                        <h4 class="entery">Mobile Recharge</h4>
+                        <button class="btn entery">Mobile Recharge</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-        </div>
 </body>
 </html>
