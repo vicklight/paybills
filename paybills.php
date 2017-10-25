5bb59c77348e1764df836a0f01b7f633f00e8a11
Victor Nwauwa <victor.nwauwa93@gmail.com>
2017-10-25 07:01:22 -0700

    first commit
    add bootstrap

---
+++ paybills.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Invite friends</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/paybills.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container paybills-con">
+        <div class="col col-sm-12 col-xs-6 con">
+        <div class="row ">
+            <div class="paybills-head col-sm-8 col-xs-8 ">
+                <h3 class="panel-heading">Pay Bills</h3>
+            </div>
+        </div>
+
+        <div class="row ">
+            <div class="  paybills-body col-sm-8 col-xs-8 ">
+                <div class="row ">
+                    <div id="paybills-entery1">
+                        <h4 class="entery">CABLE TV Subscription</h4>
+                    </div>
+                    <div id="paybills-entery2">
+                        <h4 class="entery">Utility Bills</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div id="paybills-entery3">
+                        <h4 class="entery">Data Bundle</h4>
+                    </div>
+                    <div id="paybills-entery4">
+                        <h4 class="entery">Mobile Recharge</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        </div>
+</body>
+</html>
